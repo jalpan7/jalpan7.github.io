2cc3e8628a5485effadc6cc4a7d305a91bb4bc03
jalpan7 <jalpan.patel@dcmail.ca>
2024-08-01 16:27:12 -0400

    Assignment_4 (part_3)
    Added the class definition of ball directly from the script given on the MDN server website.

--- Assignment_4/Part_3/main.js
+++ Assignment_4/Part_3/main.js
@@ -1,3 +1,8 @@
+// Name: Jalpan Patel
+// File: main.js
+// Date: 29 September 2024
+// Description: Let bounce some balls.
+
 // setup canvas
 
 const canvas = document.querySelector("canvas");
@@ -6,7 +11,7 @@ const ctx = canvas.getContext("2d");
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
-// function to generate random number
+// function to generate random number among the min and max.
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -16,4 +21,17 @@ function random(min, max) {
 
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+// Ball class definition taken directluy from the script given.
+class Ball {
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+
+
 }
