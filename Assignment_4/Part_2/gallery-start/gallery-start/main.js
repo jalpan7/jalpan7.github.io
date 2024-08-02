61543dac978dab97fe3819a9e07b4db109c477ba
jalpan7 <jalpan.patel@dcmail.ca>
2024-08-01 22:52:14 -0400

    Assignment_4 (Part_2)
    added the pics description to the const imageAlts

--- Assignment_4/Part_2/gallery-start/gallery-start/main.js
+++ Assignment_4/Part_2/gallery-start/gallery-start/main.js
@@ -2,6 +2,7 @@
 // File: main.js
 // Date: 01, August 2024
 // Description: Image Gallery
+
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
@@ -13,11 +14,11 @@ const imageFiles = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
 const imageAlts = {
-    "pic1.jpg":
-    "pic2.jpg":
-    "pic3.jpg":
-    "pic4.jpg":
-    "pic5.jpg":
+    "pic1.jpg": "Closeup of a blue human eye",
+    "pic2.jpg": "Rock that looks like a wave",
+    "pic3.jpg": "Purple and white pansies",
+    "pic4.jpg": "Section of a wall from a pharaoh\'s tomb",
+    "pic5.jpg": "Large moth on a leaf"
 };
 
 /* Looping through images */
