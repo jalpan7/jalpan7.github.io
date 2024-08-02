e0567e57fedb1f9c735c8495a1889137168cf7b5
jalpan7 <jalpan.patel@dcmail.ca>
2024-08-01 23:12:51 -0400

    Assignment_4 (Part_2)
    added the click event listener to each thumbnail

--- Assignment_4/Part_2/gallery-start/gallery-start/main.js
+++ Assignment_4/Part_2/gallery-start/gallery-start/main.js
@@ -10,22 +10,33 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const imageFiles = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+const image = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
 const imageAlts = {
     "pic1.jpg": "Closeup of a blue human eye",
     "pic2.jpg": "Rock that looks like a wave",
     "pic3.jpg": "Purple and white pansies",
-    "pic4.jpg": "Section of a wall from a pharaoh\'s tomb",
-    "pic5.jpg": "Large moth on a leaf"
+    "pic4.jpg": "Animal",
+    "pic5.jpg": "Butterfly"
 };
 
 /* Looping through images */
+for (let i = 0; i < imageFilenames.length; i++) {
+// get the current filename from ther array
+    const filename = imageFilenames[i];
 
+// create a new image elements.
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
 
+newImage.setAttribute('src', image);
+newImage.setAttribute('alt', imageAlts);
+thumbBar.appendChild(newImage);
+//adding the click event listener to each thumbnail image
+newImage.addEventListener('click', (e) => {
+    // Set the displayed image source and alt text to match the clicked thumbnail
+    displayedImage.setAttribute('src', e.target.getAttribute('src'));
+    displayedImage.setAttribute('alt', e.target.getAttribute('alt'));
+  });
+});
 /* Wiring up the Darken/Lighten button */
