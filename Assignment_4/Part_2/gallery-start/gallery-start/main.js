2a5e65ccbd8fb2fb60f722583c9df08207c36f05
jalpan7 <jalpan.patel@dcmail.ca>
2024-08-01 22:48:40 -0400

    Assignment_4 (part_2)
    added the picture to the array name const ImageFiles

--- Assignment_4/Part_2/gallery-start/gallery-start/main.js
+++ Assignment_4/Part_2/gallery-start/gallery-start/main.js
@@ -9,8 +9,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFiles = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
+const imageAlts = {
+    "pic1.jpg":
+    "pic2.jpg":
+    "pic3.jpg":
+    "pic4.jpg":
+    "pic5.jpg":
+};
 
 /* Looping through images */
 
